1559752463fea1925390570adc9f2469bdbadc4e
Winnycloud27 <winnyezinne27@gmail.com>
2023-10-07 02:06:37 +0100

    printing both cases to the console using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void upper_lower_case(void);
+
+/**
+ * main - Entry point, it will generate and print a random number
+ * to the console by determine what they are
+ * Return: 0 on success. End program
+*/
+int main(void)
+{
+	upper_lower_case();
+	putchar('\n');
+	return (0);
+}
+
+/**
+ * upper_lower_case - A function that prints alphabets both
+ * in upper and lower case to the console when called
+*/
+void upper_lower_case(void)
+{
+	int lower_case = 97;
+	int upper_case = 65;
+
+	for (; lower_case <= 122; lower_case++)
+	{
+		putchar(lower_case);
+	}
+
+	for (; upper_case <= 90; upper_case++)
+	{
+		putchar(upper_case);
+	}
+}

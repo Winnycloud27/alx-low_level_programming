15d2374fd7b03ed696f10c922c600a1a32e61e46
Winnycloud27 <winnyezinne27@gmail.com>
2023-10-07 01:51:41 +0100

    A program that prints alphabets to the console

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void alpha(void);
+
+/**
+ * main - Entry point, it will generate and print a random number
+ * to the console by determine what they are
+ * Return: 0 on success. End program
+*/
+int main(void)
+{
+	alpha();
+	putchar('\n');
+	return (0);
+}
+
+/**
+ * alpha - prints alphabets to the console when called
+*/
+void alpha(void)
+{
+	char letters = 'a';
+
+	for (; letters <= 'z'; letters++)
+	putchar(letters);
+}

253db3069097b895dc343a9492b29fedff7f2e03
Winnycloud27 <winnyezinne27@gmail.com>
2023-10-12 08:51:38 +0100

    updated

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,10 +5,12 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <ctype.h>
 
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+int _islower(int c);
 
 
 #endif

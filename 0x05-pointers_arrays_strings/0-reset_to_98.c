1d2be80834b646cfcc6d9214cb66ac147dcf5c4b
Winnycloud27 <winnyezinne27@gmail.com>
2023-10-16 22:03:33 +0100

    updated

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that reset the value of a pointer
+ *
+ * @n: The pointer being resetted.
+*/
+
+void reset_to_98(int *n)
+{
+	int c = 98;
+	*n = c;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+#include <time.h>
+#include <unistd.h>
+#include <ctype.h>
+
+void reset_to_98(int *n);
+
+#endif

2295afc1cd434dc9c29a9b45f18a6e2376be914a
Winnycloud27 <winnyezinne27@gmail.com>
2023-10-21 16:18:52 +0100

    corrected

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -19,3 +19,22 @@ void print_rev(char *s)
 
 	_putchar('\n');
 }
+
+/**
+ * _strlen - A function that prints the length of a string
+ * @s: The string being checked.
+ *
+ * Return: Returns the length of the string.
+*/
+int _strlen(char *s)
+{
+	int x = 0;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	return (x);
+}
+

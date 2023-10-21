88e9855eb0ddfb2c819d768b568a671292fc5fd2
Winnycloud27 <winnyezinne27@gmail.com>
2023-10-21 16:21:40 +0100

    corrected

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -23,7 +23,23 @@ void rev_string(char *s)
 		x++;
 		c--;
 	}
+}
 
+/**
+ * _strlen - A function that prints the length of a string
+ * @s: The string being checked.
+ *
+ * Return: Returns the length of the string.
+*/
 
+int _strlen(char *s)
+{
+	int x = 0;
 
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	return (x);
 }

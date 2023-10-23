2df5ab4a5db53d8a7f716c2144fa04ac66f8fb4b
Winnycloud27 <winnyezinne27@gmail.com>
2023-10-23 09:59:01 +0100

    A function that split a string into half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,31 @@
 #include "main.h"
 
 /**
+ * puts_half - A function that prints half of a string
+ * @str: The str being divide and printed
  *
+ * Return: void
 */
 void puts_half(char *str)
 {
-    int i;
-    int j = 0;
+	int j;
+	int length = 0;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        _putchar(* (str + i));
-    }
+	while (length >= 0)
+	{
+		if (str[length] == '\0')
+			break;
+		length++;
+	}
 
-    _putchar('\n');
+	if (length % 2 == 1)
+		j = length / 2;
+
+	else
+		j = (length - 1) / 2;
+
+	for (j++; j < length; j++)
+		_putchar(str[j]);
+
+	_putchar('\n');
 }

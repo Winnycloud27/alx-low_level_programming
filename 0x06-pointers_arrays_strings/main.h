ce47726b760206c75572f556bf795be7fc4447eb
Winnycloud27 <winnyezinne27@gmail.com>
2023-10-24 23:42:43 +0100

    corrected

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,9 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 
+#include <unistd.h>
+
+int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
@@ -9,6 +12,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *c);
-char *rot13(char *);
+char *rot13(char *s);
+void print_number(int n);
 
 #endif

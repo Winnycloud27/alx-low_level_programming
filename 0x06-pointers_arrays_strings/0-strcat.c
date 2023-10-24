e9930fa022ecd4ab05ebee6945e3c42fcad6e18c
Winnycloud27 <winnyezinne27@gmail.com>
2023-10-24 14:34:16 +0100

    A function that joins two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,22 +9,17 @@
 */
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j; /*This serves as an iterator and is initialized to zero*/
+	int i = 0, j = 0; /*This serves as an iterator and is initialized to zero*/
 
 	while (dest[i] != '\0')
 	{
 		i++;
 	}
 
-	i = 8; /*This is the last value of index value of dest[i] in the memory*/
-
-	j = 0; /*This serves as the iterator for the second char pointer*/
-
-	while (src[j] != '\0')
+	for (; src[j] != '\0'; j++)
 	{
 		dest[i] = src[j];
 		i++;
-		j++;
 	}
 
 	dest[i] = '\0';

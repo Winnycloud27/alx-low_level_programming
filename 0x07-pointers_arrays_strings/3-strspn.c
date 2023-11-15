2e9840085e0c92306bd6bf80d176e4824a2738c1
Winnycloud27 <winnycloudconcept@gmail.com>
2023-11-15 01:55:05 +0100

    Substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ * @s: char string array
+ * @accept: char array to check bytes with
+ * Return: Number of bytes in the intial segment of `s`
+ */
+
+ unsigned int _strspn(char *s, char *accept)
+ {
+	 int c1, c2, i;
+	 
+	 c1 = 0;
+	 i = 0;
+	 
+	 while (s[c1] != '\0')
+	 {
+		 if(s[c1] != 32)
+		 {
+			 for (c2 = 0; accept[c2] != '\0'; c2++)
+			 {
+				 if(s[c1] == accept[c2])
+					 i++;
+			 }
+		 }
+		 c1++;
+	 }
+	 return (i);
+ }

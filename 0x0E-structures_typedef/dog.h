8d6cf85ca0a6fc157ecba728d93eef884bc66380
Winnycloud27 <winnyezinne27@gmail.com>
2023-11-28 20:42:26 +0100

    struct dog definition

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - this is the data type
+ * @name: member 1 of the struct
+ * @age: member 2 of the struct
+ * @owner: member 3 of the struct
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

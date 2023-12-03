4cbc623bf1588456be4b71d129de159dad55dbcd
Winnycloud27 <winnyezinne27@gmail.com>
2023-12-03 17:41:52 +0100

    A function that prints integer

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * print_numbers - prints numbers adding seperator
+ * @n: count of parameters
+ * @separator: the separator
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	int digit;
+
+	va_list print_int;
+
+	va_start(print_int, n);
+	for (i = 0; i < n; i++)
+	{
+		if (separator != NULL)
+		{
+			digit = va_arg(print_int, int);
+			printf("%d", digit);
+			if (i < n - 1)
+				printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(print_int);
+}
